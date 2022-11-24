50d80f1bd95c44d18f760e1ea64d50f9ea3411de
John Paul Wile <botsone@gmail.com>
2022-11-23 20:55:25 -0700

    chore: modified logging

--- commands/randomUser.js
+++ commands/randomUser.js
@@ -5,11 +5,11 @@ module.exports = {
 		.setName('randomuser')
 		.setDescription('Returns a random user from the server!'),
 	async execute(interaction) {
-		console.log(interaction);
-		const members = await interaction.guild.members.random();
-		console.log(members.user);
-		const randomMember = members.user;
-		console.log(randomMember);
-		await interaction.reply(`The random user is ${randomMember}!`);
+		console.log('interaction: ', interaction);
+		// const members = await interaction.guild.members.cache;
+		// console.log(members.user);
+		// const randomMember = members.user;
+		// console.log(randomMember);
+		// await interaction.reply(`The random user is ${randomMember}!`);
 	},
 };
