2a6be6bf151b39de495ad470702e9e9d9db28e79
John Paul Wile <botsone@gmail.com>
2022-11-26 00:59:55 -0700

    fix: fixed random user command

--- commands/randomUser.js
+++ commands/randomUser.js
@@ -6,10 +6,10 @@ module.exports = {
 		.setDescription('Returns a random user from the server!'),
 	async execute(interaction) {
 		console.log('interaction: ', interaction);
-		const members = await interaction.member.guild.members.cache;
+		const members = await interaction.member.guild.members.fetch();
 		console.log(members);
 		const randomMember = members.random();
 		console.log(randomMember);
-		// await interaction.reply(`The random user is ${randomMember}!`);
+		await interaction.reply(`The random user is ${randomMember}!`);
 	},
 };
